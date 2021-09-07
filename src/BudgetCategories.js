f733708105aae661a3818860a9201846a033c927
Adam Pogwizd <adampog77@gmail.com>
2021-09-06 21:28:54 -0600

    round down transaction sum on categories page

--- src/BudgetCategories.js
+++ src/BudgetCategories.js
@@ -75,7 +75,7 @@ function BudgetCategories({menuState, hideMenu}) {
               <span key={i} className='budgetCategoryContainerWrap'>
                 <div className='budgetCategoryContainer' onClick={() => onClickCategory(category.id)}>
                   <p className='categoryLabel'>{category.label}</p>
-                  <p className={`categoryAmount ${amountClass(category)}`}>{category.spent} / {category.monthlyAmount}</p>
+                  <p className={`categoryAmount ${amountClass(category)}`}>{Math.floor(category.spent)} / {category.monthlyAmount}</p>
                   <div className='trashContainer' onClick={(e) => onClickDelete(e, category.id)}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className={`trash ${menuState}`} viewBox="0 0 16 16">
                       <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
